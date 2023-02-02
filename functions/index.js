73251fb869f0b8ce048c62d8418b0525c1b1d7bf
saziz <shan.azizh@gmail.com>
2023-02-02 23:23:27 +0100

    user on create, init empty fields

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,10 @@
 const functions = require("firebase-functions");
 const fetch = require("node-fetch");
+const firebase = require('firebase-admin');
+
+firebase.initializeApp()
+
+const firestore = firebase.firestore()
 
 const appIds = fetch(
         "https://api.steampowered.com/ISteamChartsService/GetMostPlayedGames/v1/",
@@ -25,24 +30,71 @@ const appDetail = (appId) => fetch(
             title: res[appId]['data']['name'],
             editor: res[appId]['data']['publishers'][0],
             price: res[appId]['data']['price_overview'] && res[appId]['data']['price_overview']['initial_formatted'] ? res[appId]['data']['price_overview']['initial_formatted'] : 'free',
-            description: res[appId]['data']['detailed_description'],
-            gbImage: res[appId]['data']['background']
+            //description: res[appId]['data']['detailed_description'],
+            bgImage: res[appId]['data']['background']
         }
     })
+
+const appSearch = (text) => fetch(
+        `https://steamcommunity.com/actions/SearchApps/${text}`,
+        {
+            method: 'GET'
+        }
+    )
+    .then(res => res.json())
+    .then(res => {
+        return res.map((r) => r['appid'])
+    }) 
 
 
 exports.games = functions.https.onRequest(async (req, res) => {
     try {
         let response = await appIds
-        let games = await Promise.all(response.map(async (appId) => {
-            console.log(appId)
-            return await appDetail(appId).catch(() => null)
-        }))
-        console.log(games);
-        res.status(200).send(JSON.stringify(games));
+        res.status(200).send(JSON.stringify(response));
     }
     catch (err) {
         console.log(err);
 		res.status(500).json({msg: `Error.`});
     }
 })
+
+exports.details = functions.https.onRequest(async (req, res) => {
+    try {
+        const id = req.query.appId;
+        if(!id){
+            res.status(400).json({msg: `No id.`});
+        }
+        let response = await appDetail(id);
+        res.status(200).send(JSON.stringify(response));
+    }
+    catch (err) {
+        console.log(err);
+		res.status(500).json({msg: `Error.`});
+    }
+})
+
+exports.search = functions.https.onRequest(async (req, res) => {
+    try {
+        const text = req.query.text
+        if(!text) {
+            res.status(400).json({msg: `No id.`});  
+        }
+        let response = await appSearch(text)
+        res.status(200).send(JSON.stringify(response))
+    }
+    catch (err) {
+        console.log(err);
+		res.status(500).json({msg: `Error.`});
+    }
+})
+
+exports.onUserCreate = functions.auth.user().onCreate((user) => {
+    return firestore.collection('users').doc(user.uid).set(
+        {
+            liked: [],
+            wished: []
+        }
+    )
+    .then(() => console.log("User document created successfuly"))
+    .catch((err) => console.log("User document creation failer " + err))
+ }) 
